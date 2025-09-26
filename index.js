a617489273e9e44359d3e9aea42e5f7221dbd90a
rml-x <cristiane.xavier@aluno.riogrande.ifrs.edu.br>
2025-09-26 21:16:38 +0000

    fhuihhf

---
+++ index.js
@@ -0,0 +1,62 @@
+import {pop, next, middle} from './org.js'
+
+var numero = 9
+
+var texto = 'adt'
+
+var lista = [2, 3, 8, 2, 4]
+
+var booleano = true
+
+ 
+try {
+    console.log(pop(numero))
+} 
+catch(exception) {
+    console.log("aconteceu um problema: " + exception)
+}
+
+/*
+console.log(pop(texto))
+
+console.log(pop(lista))
+
+console.log(pop(booleano))
+
+ 
+
+console.log(pop(pop(pop(pop(pop(numero))))))
+
+console.log(pop(pop(pop(pop(pop(texto))))))
+
+console.log(pop(pop(pop(pop(pop(lista))))))
+
+ 
+
+console.log(next(numero))
+
+console.log(next(texto))
+
+console.log(next(lista))
+
+console.log(next(booleano))
+
+ 
+
+console.log(next(next(next(numero))))
+
+console.log(next(next(next(texto))))
+
+console.log(next(next(next(lista))))
+
+ 
+
+console.log(middle(numero))
+
+console.log(middle(texto))
+
+console.log(middle(lista))
+
+console.log(middle(booleano))
+*/
+ 
---
+++ org.js
@@ -0,0 +1,19 @@
+
+function pop(a) { // see: https://www.google.com/search?q=pop+operation
+    if (typeof(a) != 'Array') throw 'erro, tipo diferente de array'
+    return null
+}
+
+ 
+
+function next(a) { // open to interpretation
+    return null
+}
+
+ 
+
+function middle(a) { // open to interpretation
+    return null
+}
+
+export {pop, next, middle}
